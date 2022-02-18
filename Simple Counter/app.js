140d533d34db684736aaa29a698e37ce34ca903d
DharaneeswaranR <dharaneeshram@gmail.com>
2022-02-18 17:08:17 +0530

    Added: Save count

--- Simple Counter/app.js
+++ Simple Counter/app.js
@@ -1,8 +1,15 @@
 let count = 0
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 function increment() {
     count++
     countEl.innerText = count
     console.log("count = " + count)
+}
+
+function save() {
+    let saveCount = count + " - "
+    saveEl.innerText += saveCount
+    console.log("Saved")
 }
